2bd53c53ee2a329b92c425b8d0e9fd70f9a36eee
Jordan Schultz <jmschultz@ucdavis.edu>
2023-07-13 14:44:28 -0700

    Added login page view, phone number verification UI

--- group-chat-therapy-ui/src/views/login/login.jsx
+++ group-chat-therapy-ui/src/views/login/login.jsx
@@ -1,13 +1,54 @@
 import React, {useState} from "react";
 import {Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './login.css';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/bootstrap.css'
 
 export default function Login() {
-    const [count, setCount] = useState(0)
-
+    const [phone, setPhone] = useState('')
     return (
         <>
-            <h1>Login page</h1>
-            <Link to={"/chat"}>Click</Link>
+            <Container className="h-100 ct">
+                <Row className="align-items-center h-100">
+                    <Col md={{ span: 6, offset: 3 }}>
+
+                        <Card className="card-body">
+                            <h2 className="text-center">Login</h2>
+                            <div className="text-center mb-5">
+                                <img src="https://storage.googleapis.com/group-chat-therapy/stanford-logo.svg" alt="logo-shield" className="logo-shield my-3"/>
+                            </div>
+
+                            <InputGroup className="mb-3">
+                                <InputGroup.Text id="sunet">@</InputGroup.Text>
+                                <Form.Control
+                                    placeholder="Sunet"
+                                    aria-label="Sunet"
+                                    aria-describedby="sunet"
+                                />
+                            </InputGroup>
+                            <InputGroup className="mb-3">
+                                <PhoneInput
+                                    country={'us'}
+                                    value={phone}
+                                    onChange={phone => setPhone(phone )}
+                                />
+                            </InputGroup>
+                            <div className="d-grid gap-2 mb-5">
+                                <Button className="submit">Submit</Button>
+                            </div>
+                            <p className="text-center"><i>Not registered? Contact <a href="#">here</a></i> </p>
+                            {/*<Link to={"/chat"}>Click</Link>*/}
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- pages/root.php
+++ pages/root.php
@@ -11,6 +11,13 @@
     <title>Vite + React</title>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.1.slim.min.js" integrity="sha256-w8CvhFs7iHNVUtnSP0YKEg00p9Ih13rlL9zGqvLdePA=" crossorigin="anonymous"></script>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+        crossorigin="anonymous"
+    />
+    <link href="https://fonts.cdnfonts.com/css/source-sans-pro" rel="stylesheet">
 
     <?php
         $module->injectJSMO();
