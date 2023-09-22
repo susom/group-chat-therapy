85306c7ffa56c7ba2d0ea474fcffbc7532eed729
Jordan Schultz <jmschultz@ucdavis.edu>
2023-09-21 17:53:18 -0700

    Added completed skeleton

---
+++ group-chat-therapy-ui/src/views/completed/completed.jsx
@@ -0,0 +1,29 @@
+import React, {useContext, useState} from "react";
+import {Link, Navigate, useNavigate, useLocation} from "react-router-dom";
+
+
+import {NavHeader} from "../../components/NavHeader/navheader.jsx";
+import SurveyList from '../../components/SurveyList/surveylist.jsx';
+
+import {SessionContext} from "../../contexts/Session.jsx";
+import './completed.css';
+
+export default function Completed() {
+    const session_context   = useContext(SessionContext);
+    const navigate = useNavigate()
+
+    let jsmoModule;
+    if (import.meta?.env?.MODE !== 'development')
+        jsmoModule = ExternalModules.Stanford.GroupChatTherapy
+
+
+    return (
+        <>
+            <NavHeader/>
+            <div>
+                Thank you for completing the chat session!
+            </div>
+            <SurveyList/>
+        </>
+    )
+}
